d51bbbc98f7164b63bc1680b2ae54369a0a8e8dd
Pete A <peter.ayresmusic@gmail.com>
2019-01-08 02:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function increaseRankBy(n){
           var li = children[j];
           var num = li.innerHTML;
           var numInt = parseInt(num);
-          console.log( n + numInt); 
+            li.innerHTML =  n + numInt;  
         }
 
     }
